7904f7b2ab80dfde77952f4d92a0a2faaef307b5
PolarisGang <36569529+PolarisGang@users.noreply.github.com>
2018-07-27 15:22:32 +0200

    SourceEngine Chat
    Sparetime fun

---
+++ SourceEngine Chat/SourceEngine Chat/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ChatConfig = New System.Windows.Forms.TextBox()
+        Me.KeyToBind = New System.Windows.Forms.TextBox()
+        Me.ChatMsg = New System.Windows.Forms.TextBox()
+        Me.AddBtn = New System.Windows.Forms.Button()
+        Me.MyDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'ChatConfig
+        '
+        Me.ChatConfig.Location = New System.Drawing.Point(12, 12)
+        Me.ChatConfig.Multiline = True
+        Me.ChatConfig.Name = "ChatConfig"
+        Me.ChatConfig.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.ChatConfig.Size = New System.Drawing.Size(1006, 559)
+        Me.ChatConfig.TabIndex = 0
+        '
+        'KeyToBind
+        '
+        Me.KeyToBind.Location = New System.Drawing.Point(13, 578)
+        Me.KeyToBind.Name = "KeyToBind"
+        Me.KeyToBind.Size = New System.Drawing.Size(100, 20)
+        Me.KeyToBind.TabIndex = 1
+        '
+        'ChatMsg
+        '
+        Me.ChatMsg.Location = New System.Drawing.Point(119, 578)
+        Me.ChatMsg.Name = "ChatMsg"
+        Me.ChatMsg.Size = New System.Drawing.Size(809, 20)
+        Me.ChatMsg.TabIndex = 2
+        '
+        'AddBtn
+        '
+        Me.AddBtn.Location = New System.Drawing.Point(943, 576)
+        Me.AddBtn.Name = "AddBtn"
+        Me.AddBtn.Size = New System.Drawing.Size(75, 23)
+        Me.AddBtn.TabIndex = 3
+        Me.AddBtn.Text = "Add"
+        Me.AddBtn.UseVisualStyleBackColor = True
+        '
+        'MyDialog
+        '
+        Me.MyDialog.RootFolder = System.Environment.SpecialFolder.MyComputer
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1030, 608)
+        Me.Controls.Add(Me.AddBtn)
+        Me.Controls.Add(Me.ChatMsg)
+        Me.Controls.Add(Me.KeyToBind)
+        Me.Controls.Add(Me.ChatConfig)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "SourceEngine Chat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ChatConfig As TextBox
+    Friend WithEvents KeyToBind As TextBox
+    Friend WithEvents ChatMsg As TextBox
+    Friend WithEvents AddBtn As Button
+    Friend WithEvents MyDialog As FolderBrowserDialog
+End Class
---
+++ SourceEngine Chat/SourceEngine Chat/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+Public Class Form1
+
+    <DllImport("user32.dll")>
+    Public Shared Function GetAsyncKeyState(ByVal vKey As Keys) As Short
+    End Function
+
+    'Variables
+    Private GamePath() As String = New String() {
+        "Counter-Strike Global Offensive\csgo\cfg",
+        "Counter-Strike Source\cstrike\cfg"}
+    Private ConfigDir As String
+    Private KeyBind As String
+    Private ChatCount As Integer = 0
+    Private WelcomeMsg() As String = New String() {"Clear" & vbCrLf &
+        "echo PolarisGang: SourceEngine Chat" & vbCrLf &
+        "echo -----------------------------------------------------------" & vbCrLf &
+        "echo Spam chat with -> ", vbCrLf &
+        "echo -----------------------------------------------------------" & vbCrLf &
+        "echo Source -> Github.com/PolarisGang"}
+
+    Private Function FindSteam() As Boolean
+        For Each p As Process In Process.GetProcesses()
+            If Not p.ProcessName = "Steam" Then Continue For
+            Dim file As String
+            Try
+                file = p.Modules(0).FileName
+            Catch ex As Exception
+
+            End Try
+            file = file.Replace("Steam.exe", "steamapps\common\")
+            Try
+                If IO.Directory.Exists(file) Then
+                    ConfigDir = file
+                    Return True
+                End If
+            Catch x2 As Exception
+                Return False
+            End Try
+        Next
+        End
+        Return False
+    End Function
+
+    Private Sub ChatMsg_KeyDown(sender As Object, e As KeyEventArgs) Handles ChatMsg.KeyDown
+        If GetAsyncKeyState(Keys.Enter) <> 0 Then AddBtn.PerformClick()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not FindSteam() Then
+            MyDialog.ShowDialog()
+            ConfigDir = MyDialog.SelectedPath
+        End If
+        For i As Integer = 0 To GamePath.Length - 1
+            If IO.File.Exists(ConfigDir + GamePath(i) + "\ChatSpam.cfg") Then
+                Using myReader As New IO.StreamReader(ConfigDir + GamePath(i) + "\ChatSpam.cfg")
+                    ChatConfig.Text = myReader.ReadToEnd
+                End Using
+                Dim temp = ChatConfig.Text.Split(New String() {Environment.NewLine}, StringSplitOptions.None)
+                'Find current chatcount
+                Dim temp2 = temp(temp.Length - 8).Split(" "c)
+                Dim ghetto = temp2(1).Replace(Chr(34), "")
+                ghetto = ghetto.Replace("ChatSpam", "")
+                ChatCount = Convert.ToInt32(ghetto) + 1
+                'Find key bind
+                temp2 = temp(temp.Length - 7).Split(" "c)
+                KeyToBind.Text = temp2(1).Replace(Chr(34), "")
+                KeyBind = KeyToBind.Text
+                KeyToBind.ReadOnly = True
+                'Rebuild config
+                temp(temp.Length - 8) = temp(temp.Length - 8).Replace("ChatSpam0", "ChatSpam" & ChatCount)
+                ChatConfig.Clear()
+                For i2 As Integer = 0 To temp.Length - 8
+                    ChatConfig.Text += temp(i2) + vbCrLf
+                Next
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If Not ChatConfig.Text = "" Then
+            ChatConfig.Text = ChatConfig.Text.Replace(ChatCount, 0)
+            ChatConfig.Text += "Bind " & Chr(34) & KeyBind & Chr(34) & " " & Chr(34) & "ChatSpam0" & Chr(34) & vbCrLf
+            ChatConfig.Text += WelcomeMsg(0) + KeyBind + WelcomeMsg(1)
+            If GamePath.Length > 1 Then
+                For i As Integer = 0 To GamePath.Length - 1
+                    Using MyWriter As New IO.StreamWriter(ConfigDir + GamePath(i) + "\ChatSpam.cfg")
+                        MyWriter.Write(ChatConfig.Text)
+                    End Using
+                Next
+            Else
+                Using MyWriter As New IO.StreamWriter(ConfigDir + GamePath(0) + "\ChatSpam.cfg")
+                    MyWriter.Write(ChatConfig.Text)
+                End Using
+            End If
+
+        End If
+    End Sub
+
+    Private Sub AddBtn_Click(sender As Object, e As EventArgs) Handles AddBtn.Click
+        If ChatConfig.Text = "" Then
+            If Not KeyToBind.Text = "" AndAlso Not ChatMsg.Text = "" Then
+                KeyToBind.ReadOnly = True
+                KeyBind = KeyToBind.Text
+            End If
+            AddChat(ChatMsg.Text, KeyBind, ChatCount)
+            ChatCount += 1
+            ChatMsg.Clear()
+        Else
+            If Not ChatMsg.Text = "" Then
+                AddChat(ChatMsg.Text, KeyBind, ChatCount)
+                ChatCount += 1
+                ChatMsg.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub AddChat(ByVal ChatMsg As String, ByVal KeyBind As String, ByVal AliasNumber As Integer)
+        ChatConfig.Text += "Alias " & Chr(34) & "ChatSpam" & AliasNumber.ToString & Chr(34) & " " & Chr(34) & "Say " & ChatMsg & "; Bind " & KeyBind & " ChatSpam" & (AliasNumber + 1).ToString & Chr(34) & vbCrLf
+    End Sub
+
+End Class
